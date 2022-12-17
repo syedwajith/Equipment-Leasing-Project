8369142f754eeeb01beb248c035274d0226cad6a
Syed wajith K <84230157+syedwajith@users.noreply.github.com>
2022-12-17 22:46:03 +0530

    Update models.py

--- equipment_leasing/container_leasing_app/models.py
+++ equipment_leasing/container_leasing_app/models.py
@@ -2,3 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+class LessorDetails(models.Model):
+    First_Name = models.CharField(max_length=20)
+    Last_Name = models.CharField(max_length=20)
+    Email = models.EmailField()
+    Phone_No = models.CharField(max_length=10)
+    Country = models.CharField(max_length=30)
+    State = models.CharField(max_length=30)
+    City = models.CharField(max_length=30)
+    Username = models.CharField(max_length=20)
+    Password = models.CharField(max_length=20)
+
+class LesseeDetails(models.Model):
+    First_Name = models.CharField(max_length=20)
+    Last_Name = models.CharField(max_length=20)
+    Email = models.EmailField()
+    Phone_No = models.CharField(max_length=10)
+    Company = models.CharField(max_length=50)
+    Country = models.CharField(max_length=30)
+    State = models.CharField(max_length=30)
+    City = models.CharField(max_length=30)
+    Username = models.CharField(max_length=20)
+    Password = models.CharField(max_length=20)
