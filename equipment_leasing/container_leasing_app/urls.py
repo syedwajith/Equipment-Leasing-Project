d1586a6a5e5fb5ab78561751533928b5f5305931
Syed wajith K <84230157+syedwajith@users.noreply.github.com>
2022-12-17 22:47:19 +0530

    Create urls.py

--- equipment_leasing/container_leasing_app/urls.py
+++ equipment_leasing/container_leasing_app/urls.py
@@ -19,10 +19,15 @@ from container_leasing_app import views
 
 urlpatterns = [
     path('home/', views.ContainerHome),
-    path('adminlog', views.adminlogin),
+    path('adminlog/', views.adminlogin),
     path('admin/', views.home_admin),
     path('lessorlog/', views.lessorlogin),
     path('lessor/', views.home_lessor),
     path('lesseelog/', views.lesseelogin),
-    path('lessee/', views.home_lessee)
+    path('lessee/', views.home_lessee),
+    path('adminhome/', views.adminhome),
+    path('lessorreg/', views.lessor_register),
+    path('lessorlogreg/', views.lessor_log_reg),
+    path('lesseereg/', views.lessee_register),
+    path('lesseelogreg/', views.lessee_log_reg),
 ]
